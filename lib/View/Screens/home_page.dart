844b1099ec46c6b4ecec1707fbf89e830a4e7e91
guptakaran11 <gupta.karan1162@gmail.com>
2024-05-07 12:13:56 +0530

    step1 done

---
+++ lib/View/Screens/home_page.dart
@@ -0,0 +1,29 @@
+//* Packages
+import 'package:device_frame/device_frame.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          DeviceFrame(
+            device: Devices.ios.iPhone13,
+            screen: const Center(
+              child: Text(
+                'Hello World',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
