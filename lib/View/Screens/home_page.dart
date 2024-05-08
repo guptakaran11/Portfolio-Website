ee3d8279d37d3d12c8863c96a5a7b99233d065e9
guptakaran11 <gupta.karan1162@gmail.com>
2024-05-08 12:48:34 +0530

    background complete

--- lib/View/Screens/home_page.dart
+++ lib/View/Screens/home_page.dart
@@ -1,26 +1,48 @@
 //* Packages
 import 'package:device_frame/device_frame.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          DeviceFrame(
-            device: Devices.ios.iPhone13,
-            screen: const Center(
-              child: Text(
-                'Hello World',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                colors: [
+                  Colors.blue,
+                  Colors.black45,
+                ],
               ),
             ),
+          ),
+          SvgPicture.asset(
+            "assets/images/CloudyBlue.svg",
+            height: size.height,
+            fit: BoxFit.cover,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DeviceFrame(
+                device: Devices.ios.iPhone13,
+                screen: const Center(
+                  child: Text(
+                    'Hello World',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
